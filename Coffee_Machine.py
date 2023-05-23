1f0a5f36e9e234048c41264aead21f653a80a0dc
Paul Quintanilla <paulq10@gmail.com>
2023-05-22 18:01:10 -0700

    calculate function fixed

--- Coffee_Machine.py
+++ Coffee_Machine.py
@@ -64,6 +64,9 @@ def cappuccino_drink(water,milk,coffee):
         print("Sorry there isn't enough coffee")
         return 0
 
+def calculate(quarters_input,dimes_input,nickels_input,pennies_input):
+    money_inserted=quarters_input*0.25+dimes_input*0.10+nickels_input*0.05+pennies_input*0.01
+    return float(money_inserted)
 
 
 
@@ -89,27 +92,47 @@ while machine_on==True:
     if order=="latte":
         message=latte_drink(water=resources["water"],milk=resources["milk"],coffee=resources["coffee"])
         if not message== 0:
-            resources['water']-=200
-            resources["milk"]-=150
-            resources["coffee"]-=24
-            resources["money"]+=2.5
-            print(f"Enjoy your {order}")
+            quarters=float(input("how many quarters?: "))
+            dimes=float(input("how many dimes?: "))
+            nickles=float(input("how many nickels?: "))
+            pennies=float(input("how many pennies?: "))
+            total=calculate(quarters_input=quarters,dimes_input=dimes,nickels_input=nickles,pennies_input=pennies)
+            if total>=2.50:
+                resources['water']-=200
+                resources["milk"]-=150
+                resources["coffee"]-=24
+                resources["money"]+=total
+                
+                print(f"Enjoy your {order}")
         
     if order=="espresso":
         message=espresso_drink(water=resources["water"],coffee=resources["coffee"])
         if not message== 0:
-            resources['water']-=50
-            resources["coffee"]-=18
-            resources["money"]+=1.5
-            print(f"Enjoy your {order}")
+            quarters=float(input("how many quarters?: "))
+            dimes=float(input("how many dimes?: "))
+            nickles=float(input("how many nickels?: "))
+            pennies=float(input("how many pennies?: "))
+            total=calculate(quarters_input=quarters,dimes_input=dimes,nickels_input=nickles,pennies_input=pennies)
+            if total>=1.50:
+                resources['water']-=50
+                resources["coffee"]-=18
+                resources["money"]+=total
+            
+                print(f"Enjoy your {order}")
     if order=="cappuccino":
         message=cappuccino_drink(water=resources["water"],milk=resources["milk"],coffee=resources["coffee"])
         if not message== 0:
-            resources['water']-=250
-            resources["milk"]-=100
-            resources["coffee"]-=24
-            resources["money"]+=3.0
-            print(f"Enjoy your {order}")
+            quarters=float(input("how many quarters?: "))
+            dimes=float(input("how many dimes?: "))
+            nickles=float(input("how many nickels?: "))
+            pennies=float(input("how many pennies?: "))
+            total=calculate(quarters_input=quarters,dimes_input=dimes,nickels_input=nickles,pennies_input=pennies)
+            if total>=3.00:
+                resources['water']-=250
+                resources["milk"]-=100
+                resources["coffee"]-=24
+                resources["money"]+=3.0
+                print(f"Enjoy your {order}")
 
 
         
