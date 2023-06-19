2175ffbb33a9d8b816d0b53c628927fb38b5c957
Paul Quintanilla <paulq10@gmail.com>
2023-06-19 03:49:40 -0700

    format

--- 2023_Final_OOP_QUIZ_GAME_June_18th/data.py
+++ 2023_Final_OOP_QUIZ_GAME_June_18th/data.py
@@ -1,14 +1,85 @@
 question_data = [
-{"text": "A slug's blood is green.", "answer": "True"},
-{"text": "The loudest animal is the African Elephant.", "answer": "False"},
-{"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
-{"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
-{"text": "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", "answer": "True"},
-{"text": "In London, UK, if you happen to die in the House of Parliament, you are entitled to a state funeral.", "answer": "False"},
-{"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
-{"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
-{"text": "Google was originally called 'Backrub'.", "answer": "True"},
-{"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
-{"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
-{"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
+	
+response_code	0
+results	
+0	
+category	"Entertainment: Film"
+type	"boolean"
+difficulty	"easy"
+question	"Leonardo DiCaprio won an Oscar for Best Actor in 2004&#039;s &quot;The Aviator&quot;."
+correct_answer	"False"
+incorrect_answers	
+0	"True"
+1	
+category	"Entertainment: Film"
+type	"boolean"
+difficulty	"easy"
+question	"The film &quot;2001: A Space Odyssey&quot; was released on December 31st, 2000."
+correct_answer	"False"
+incorrect_answers	
+0	"True"
+2	
+category	"Entertainment: Film"
+type	"boolean"
+difficulty	"easy"
+question	"Matt Damon played an astronaut stranded on an extraterrestrial planet in both of the movies Interstellar and The Martian."
+correct_answer	"True"
+incorrect_answers	
+0	"False"
+3	
+category	"Entertainment: Film"
+type	"boolean"
+difficulty	"easy"
+question	"The word &quot;Inception&quot; came from the 2010 blockbuster hit &quot;Inception&quot;."
+correct_answer	"False"
+incorrect_answers	
+0	"True"
+4	
+category	"Entertainment: Film"
+type	"boolean"
+difficulty	"easy"
+question	"Samuel L. Jackson had the words, &#039;Bad Motherf*cker&#039; in-scripted on his lightsaber during the filming of Star Wars."
+correct_answer	"True"
+incorrect_answers	
+0	"False"
+5	
+category	"Entertainment: Film"
+type	"boolean"
+difficulty	"easy"
+question	"In Alfred Hitchcock&#039;s film &#039;Psycho&#039; it is said he used chocolate syrup to simulate the blood in the famous shower scene from "
+correct_answer	"True"
+incorrect_answers	
+0	"False"
+6	
+category	"Entertainment: Film"
+type	"boolean"
+difficulty	"easy"
+question	"In the original Star Wars trilogy, David Prowse was the actor who physically portrayed Darth Vader."
+correct_answer	"True"
+incorrect_answers	
+0	"False"
+7	
+category	"Entertainment: Film"
+type	"boolean"
+difficulty	"easy"
+question	"In the original Star Wars trilogy, Alec Guinness provided the voice for Darth Vader."
+correct_answer	"False"
+incorrect_answers	
+0	"True"
+8	
+category	"Entertainment: Film"
+type	"boolean"
+difficulty	"easy"
+question	"George Lucas directed the entire original Star Wars trilogy."
+correct_answer	"False"
+incorrect_answers	
+0	"True"
+9	
+category	"Entertainment: Film"
+type	"boolean"
+difficulty	"easy"
+question	"&quot;Minions&quot; was released on the June 10th, 2015."
+correct_answer	"False"
+incorrect_answers	
+0	"True"
 ]
